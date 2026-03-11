94fd6dba6f74331c4cbab295e01d12ee138e416c
Jamie Pickett <jlizabeth03@gmail.com>
2026-03-11 11:33:22 -0400

    port error fix

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.post("/upload", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
